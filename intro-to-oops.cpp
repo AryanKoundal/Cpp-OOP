30d05c642f6e60cc8b23c40c85bc9f36448a030b
AryanKoundal <aryankoundal474@gmail.com>
2021-12-08 22:17:26 +0530

    Modified

--- intro-to-oops.cpp
+++ intro-to-oops.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-/*program for creating default constructors
+/*program for this pointer
 
 */
 class rectangle
@@ -11,10 +11,10 @@ private:
     int breadth;
 
 public:
-    rectangle()
+    rectangle(int length, int breadth)
     {
-        length = 1;
-        breadth = 1;
+        this->length = length;
+        this->breadth = breadth;
     }
     int area()
     {
@@ -23,9 +23,40 @@ public:
 };
 int main()
 {
-    rectangle r1;
+    rectangle r1(10, 5);
     cout << r1.area() << endl;
 }
+
+//////////////////////-----------------------///////////////////////////
+
+// #include <iostream>
+// using namespace std;
+
+// /*program for creating default constructors
+
+// */
+// class rectangle
+// {
+// private:
+//     int length;
+//     int breadth;
+
+// public:
+//     rectangle()
+//     {
+//         length = 1;
+//         breadth = 1;
+//     }
+//     int area()
+//     {
+//         return length * breadth;
+//     }
+// };
+// int main()
+// {
+//     rectangle r1;
+//     cout << r1.area() << endl;
+// }
 
 //////////////////////////------------------------//////////////////////////
 
